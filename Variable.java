b9dcf1a1de1426413b760f11ee63fd831eb8effb
AdeelAslam5 <150940958+AdeelAslam5@users.noreply.github.com>
2023-11-20 17:08:03 +0500

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Integer variables
+        int age = 21;
+        int Students = 150;
+
+        // Floating-point variables
+        double temperature = 98.6;
+
+        // Displaying the values
+        System.out.println("Age: " + age);
+        System.out.println("Number of students: " + Students);
+        System.out.println("Temperature: " + temperature);
+    }
+}
